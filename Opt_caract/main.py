6c276e5681ff865fce4c8e82dbd7a6556ff9d0f0
Miguel Solarte <solartesanchezm@gmail.com>
2023-02-24 16:54:54 -0500

    codigos para obtener caracteristicas

---
+++ Opt_caract/main.py
@@ -0,0 +1,27 @@
+from using_encodec import embeddingsLabels
+from encodec import EncodecModel
+
+import pandas as pd
+import torch
+import h5py
+
+
+if __name__ == '__main__':
+
+    df = pd.read_csv("../urbansound8k/archive/UrbanSound8K.csv")
+
+    model = EncodecModel.encodec_model_24khz()
+    model.set_target_bandwidth(1.5)
+
+    path ='../urbansound8k/archive/'
+
+    X, y = embeddingsLabels(df, path, model)
+
+    X_tensor = torch.tensor(X)
+    y_tensor = torch.tensor(y)
+
+    f = h5py.File("TF_vggish.hdf5", "w")
+    dset = f.create_dataset("features", X_tensor.shape, data = X_tensor)
+    dset = f.create_dataset("labels", y_tensor.shape, data = y_tensor)
+    f.close()
+
---
+++ Opt_caract/using_encodec.py
@@ -0,0 +1,47 @@
+from encodec.utils import convert_audio
+
+import torchaudio
+import torch
+
+import numpy as np
+
+from tqdm import tqdm
+
+
+
+def embeddingsLabels(df, path, model):
+
+    y = []
+    emb = []
+
+    for index, row in tqdm(df.iterrows(), total=df.shape[0]):
+      
+        ruta = f"{path}fold{row.fold}/{row.slice_file_name}"
+        wav, sr = torchaudio.load(ruta)
+        
+        wav = convert_audio(wav, sr, model.sample_rate, model.channels)
+        wav = wav.unsqueeze(0)    
+        try:
+            embeddings = model.encoder(wav)
+            embeddings = embeddings.squeeze().T
+            
+             
+        except:
+            print(ruta)
+            break
+        
+        embeddings = torch.mean(embeddings, 0)
+        
+           
+        emb.append(embeddings.detach().to('cpu').tolist())
+        y.append(row["classID"])
+        del embeddings
+        
+        if index == 10:
+          break
+        
+    #X = torch.cat(emb,0)
+    y = np.array(y)
+        
+    return emb, y
+
